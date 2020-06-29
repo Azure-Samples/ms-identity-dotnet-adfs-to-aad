9db43b88963879808f47fefdf9660f7ba0463d40
Tiago Brenck <v-tibre@microsoft.com>
2020-06-29 11:45:28 -0700

    placeholder files

---
+++ SAML_AAD/WebApp_SAML/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_SAML.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userClaims = ClaimsPrincipal.Current.Claims.ToList();
+            return View(userClaims);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
