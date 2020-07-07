8e1e588c0b8f85e26a4b067cdd70344bcf10a676
Tiago Brenck <v-tibre@microsoft.com>
2020-07-07 10:05:44 -0700

    OIDC trial

--- SAML_AAD/WebApp_SAML/App_Start/Startup.Auth.cs
+++ SAML_AAD/WebApp_SAML/App_Start/Startup.Auth.cs
@@ -4,8 +4,12 @@ using System.Configuration;
 using System.Globalization;
 using System.Linq;
 using System.Web;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Host.SystemWeb;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 using Microsoft.Owin.Security.WsFederation;
 using Owin;
 
@@ -16,19 +20,41 @@ namespace WebApp_SAML
         private static string realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
         private static string adfsMetadata = ConfigurationManager.AppSettings["ida:ADFSMetadata"];
 
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string authority = String.Concat(aadInstance, tenantId, "/v2.0");
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+
         public void ConfigureAuth(IAppBuilder app)
         {
+            IdentityModelEventSource.ShowPII = true;
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
-            app.UseWsFederationAuthentication(
-                new WsFederationAuthenticationOptions
+            //app.UseWsFederationAuthentication(
+            //    new WsFederationAuthenticationOptions
+            //    {
+            //        Wtrealm = realm,
+            //        MetadataAddress = adfsMetadata,
+            //        Wreply = "https://localhost:44347/"
+            //    });
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
                 {
-                    Wtrealm = realm,
-                    MetadataAddress = adfsMetadata,
-                    Wreply = "https://localhost:44347/"
-                });
+                    // The `Authority` represents the v2.0 endpoint - https://login.microsoftonline.com/{tenantId}/v2.0
+                    Authority = authority,
+                    ClientId = clientId,
+                    RedirectUri = redirectUri,
+                    PostLogoutRedirectUri = redirectUri
+                    
+
+                    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/samesite/owin-samesite
+                    //CookieManager = new SameSiteCookieManager(
+                    //                 new SystemWebCookieManager())
+                }); ;
         }
     }
 }
